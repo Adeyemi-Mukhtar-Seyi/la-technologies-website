cff7a146c5a3c84dc114ceffc5588ed7542fdc17
adeyemiseyi <adeyemi.seyi.mukhtar@gmail.com>
2026-04-29 21:10:23 -1200

    Fix backend email (SMTP timeout)

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://latechnology.netlify.app",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 // ================== MONGODB ==================
